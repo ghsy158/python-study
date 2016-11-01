1ea396243249caf013d7bff736306485a3c93da2
ghsy158 <fgh326@126.com>
2016-11-01 17:09:04 +0800

    test

---
+++ scrapy-study/tutorial/tutorial/spiders/DmozSpider.py
@@ -0,0 +1,32 @@
+import scrapy
+import os
+from ..items import DmozItem
+
+print(os.getcwd())
+
+class DmozSpider(scrapy.Spider):
+    name = "dmoz"
+    allowed_domains = ["dmoz.org"]
+    start_urls = [
+        "http://www.dmoz.org/Computers/Programming/Languages/Python/",
+        # "http://www.dmoz.org/Computers/Programming/Languages/Python/Books/",
+        # "http://www.dmoz.org/Computers/Programming/Languages/Python/Resources/"
+    ]
+
+    def parse(self, response):
+        # fileName = response.url.split("/")[-2] + ".html"
+        # print("fileName=",fileName)
+        # with open(fileName, 'wb') as f:
+        #     f.write(response.body)
+        for href in response.css("ul.directory.dir-col > li > a::attr('href')"):
+            url = response.urljoin(response.url, href.extract())
+            yield scrapy.Request(url, callback=self.parse_dir_contents)
+
+    def parse_dir_contents(self, response):
+        for sel in response.xpath('//ul/li'):
+            item = DmozItem()
+            item['title'] = sel.xpath('a/text()').extract()
+            item['link'] = sel.xpath('a/@href').extract()
+            item['desc'] = sel.xpath('text()').extract()
+            # print(title,link,desc)
+            yield item
